928bac52b84dc15d3c2167bde69bccb3759ca1bf
Paul Husek <paulhusek@hotmail.com>
2024-11-02 23:11:42 -0400

    Refactoring

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,27 +2,17 @@ import Title from "./Title";
 import Count from "./Count";
 import ResetButton from "./ResetButton";
 import CountButtons from "./CountButtons";
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function Card() {
   const [count, setCount] = useState(0);
-
-  const increment = () => setCount(count + 1);
-  const decrement = () =>
-    setCount((count) => {
-      if (count > 0) {
-        return count - 1;
-      }
-      return count;
-    });
-  const reset = () => setCount(0);
 
   return (
     <div className="card">
       <Title />
       <Count number={count} />
-      <ResetButton reset={reset} />
-      <CountButtons increment={increment} decrement={decrement} />
+      <ResetButton setCount={setCount} />
+      <CountButtons setCount={setCount} />
     </div>
   );
 }
--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -1,13 +1,22 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-export default function CountButtons(props) {
-  const { increment, decrement } = props;
+export default function CountButtons({ setCount }) {
+  const decreaseCount = () => {
+    setCount((prev) => {
+      if (prev > 0) return prev - 1;
+      return prev;
+    });
+  };
+
   return (
     <div className="button-container">
-      <button className="count-btn" onClick={decrement}>
+      <button className="count-btn" onClick={decreaseCount}>
         <MinusIcon className="count-btn-icon" />
       </button>
-      <button className="count-btn" onClick={increment}>
+      <button
+        className="count-btn"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
         <PlusIcon className="count-btn-icon" />
       </button>
     </div>
